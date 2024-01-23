7cccf8bf33340d80c7ae408c96f92fe8bbfdd11f
luiz1303 <luiz.1303@hotmail.com>
2024-01-23 01:16:14 -0300

    ✨ - feat: add tailwindcss and linting

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,18 +1,34 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'standard',
+      'prettier',
     ],
-  },
-}
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'eslint-plugin-import-helpers'],
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'import-helpers/order-imports': [
+        'warn',
+        {
+          // example configuration
+          newlinesBetween: 'always',
+          groups: ['module', '/^@shared/', ['parent', 'sibling', 'index']],
+          alphabetize: { order: 'asc', ignoreCase: true },
+        },
+      ],
+    },
+  }
+  
