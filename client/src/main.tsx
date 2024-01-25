a50a68580c3c736558a5c7bfdfa86d9037060799
luiz1303 <luiz.1303@hotmail.com>
2024-01-25 18:44:03 -0300

    🐛 - fix: separate routes on a route map

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,14 +9,36 @@ import NotFound from './Pages/NotFound'
 
 import './index.css'
 
+const appRoutes = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NotFound />} />
+        {appRoutes?.map((route, index) => (
+          <Route
+            key={`app-route-${index}`}
+            path={route?.path}
+            element={route?.element}
+          />
+        ))}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
