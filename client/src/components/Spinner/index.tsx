dbbdcb642350318e774cc5f9f8af417b9f2df1ae
luiz1303 <luiz.1303@hotmail.com>
2024-01-28 16:11:09 -0300

    ✨ - feat: spinner loader

---
+++ client/src/components/Spinner/index.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex h-dvh w-dvw items-center justify-center bg-white">
+      <span className="loader"></span>
+    </div>
+  )
+}
+
+export default Spinner
--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,5 +1,6 @@
 import Button from './Button'
 import Chat from './Chat'
 import MessageBubble from './MessageBubble'
+import Spinner from './Spinner'
 
-export { Button, Chat, MessageBubble }
+export { Button, Chat, MessageBubble, Spinner }
