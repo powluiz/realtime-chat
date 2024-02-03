27798f850cf4c9468d897aa7f4673a84d5f82bbd
luiz1303 <luiz.1303@hotmail.com>
2024-02-03 17:07:24 -0300

    🐛 - fix: rename env variable

--- client/src/api/utils/http.ts
+++ client/src/api/utils/http.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from 'axios'
 import { requestInterceptor } from './interceptors'
 
 export const http: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_PUBLIC_URL,
+  baseURL: import.meta.env.VITE_SERVER_HTTP_URL,
 })
 
 http.interceptors.request.use(requestInterceptor)
